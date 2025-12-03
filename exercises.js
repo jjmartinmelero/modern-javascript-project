be6e865c2b17738fbe5c92b0ca78bd4069e9fe2e
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2025-12-03 08:56:06 +0000

    added first exercise

---
+++ exercises.js
@@ -0,0 +1,14 @@
+console.log('Init Exercices');
+
+// at in array 
+const animals = ['animal1', 'animal2', 'animal3']
+
+// get an animal:
+console.log(animals.at(0));
+
+// with negative numbers
+console.log(animals.at(-1));
+
+// get an undefined
+console.log(animals.at(5));
+
